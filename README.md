1aa73021e71705981fd2567541a3dd021c7dad0a
Rizky Iqbal <rizkiiqbal36@gmail.com>
2021-11-28 23:44:14 +0700

    docs: ✏️ add LICENSE and readme

---
+++ README.md
@@ -0,0 +1,23 @@
+<p align="center">
+  <a href="https://graphql.org/" target="blank"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/GraphQL_Logo.svg/1200px-GraphQL_Logo.svg.png" width="320" alt="GraphQL Logo" /></a>
+</p>
+
+  <p align="center">This project was built using <a href="https://graphql.org/" target="_blank">GraphQL</a> that run on top of <a href="https://www.fastify.io/" target="_blank">Fastify</a>, a <a href="http://nodejs.org" target="_blank">Node.js</a> framework for building Highly performant server-side applications.</p>
+
+
+## Description
+
+Graphql boilerplate to build scalable and maintainable app
+
+## Stay in touch
+
+- Author - [Rizky Iqbal](mailto:rizkiiqbal36@gmail.com)
+- Website - [https://rizky-iqbal.netlify.app](https://rizky-iqbal.netlify.app)
+- Instagram - [@rizky_iqbal48](https://www.instagram.com/rizky_iqbal48)
+- Twitter - [@rizkyiqbal36](https://www.twitter.com/rizkyiqbal36)
+- Github - [Rizky-Iqbal36](https://github.com/Rizky-Iqbal36)
+- Gitlab - [rizkyiqbal36](https://gitlab.com/rizkyiqbal36)
+
+## License
+
+[MIT licensed](LICENSE).
--- src/app/server/container.ts
+++ src/app/server/container.ts
@@ -1,13 +1,13 @@
-import 'module-alias/register';
-import fastify, {FastifyInstance} from 'fastify';
-import moment from 'moment';
-import 'moment-duration-format';
+import "module-alias/register";
+import fastify, { FastifyInstance } from "fastify";
+import moment from "moment";
+import "moment-duration-format";
 
-import Server from '@app/server';
+import Server from "@app/server";
 
 export default class Container {
-  private server:Server
-  public app:FastifyInstance;
+  private server: Server;
+  public app: FastifyInstance;
 
   constructor() {
     this.server = new Server(fastify());
@@ -15,19 +15,19 @@ export default class Container {
   }
 
   public async load() {
-    this.app.get('/health-check', async (req, rep)=>{
+    this.app.get("/health-check", async (req, rep) => {
       rep.send({
-        status: 'OK',
-        uptime: moment.duration(process.uptime(), 'seconds').format('h [hrs] : m [min] : s [sec]', {trim: false}),
+        status: "OK",
+        uptime: moment
+          .duration(process.uptime(), "seconds")
+          .format("h [hrs] : m [min] : s [sec]", { trim: false }),
       });
     });
-    await this.server.start()
+    await this.server.start();
   }
 }
 
-
-module.exports.startServer = async function() {
+module.exports.startServer = async function () {
   const container = new Container();
   await container.load();
 };
-
